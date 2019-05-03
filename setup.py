ceaa29e84e0398eab8eff53fe5675d85232bf135
Fabien Cazenave <fabien@cazenave.cc>
2019-05-03 22:13:58 +0200

    improved dead key support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='kalamine',
-    version='0.9.1',
+    version='0.9.2',
     description='a cross-platform Keyboard Layout Maker',
     long_description=readme,
     url='http://github.com/fabi1cazenave/kalamine',
